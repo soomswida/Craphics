7eb004bc82d9b32a079f9615e2c5f51565290ff5
Rumo <rumo@Rumo.local>
2026-06-23 08:28:26 +0900

    NFC/Minor-Code-Reordering-2

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 		// Size and Time
 		int width = atoi(argv[1]);
 		int height = atoi(argv[2]);
-		int how_long = atoi(argv[3]) * 1000;
+		int len = atoi(argv[3]) * 100;
 		char canvas[width][height];
 
 		// Object Configs
@@ -40,62 +40,45 @@ int main(int argc, char *argv[]) {
 		int y = 0; 
 		double raw_x = 0.0;
 		double raw_y = 0.0;
-		int len = 100;
+	
+		double x_scale = 1 / 4.0;
+		double y_scale = 25.0;
+
+		// Buffer 
 		int buffer[len];
 		for (int i = 0; i < len; i++) {
 			buffer[i] = 0; 
 		}
 
-		double x_scale = 1 / 15.0;
-		double y_scale = 20.0;
-
-		printf("Debug: Before Animation Loop\n");
-
 		// Animation Loop
-		while (how_long != 0) {
+		while (1) {
 			// Current Phase : Dynamic Trajectory of Trigonometric functions
 			
 			// #Current Issue# The trajectory is not printed.
 			// - 1. Make another simple trajectory generator.
 			// - 2. Test it. 
 
-
-			printf("Debug: Animation Loop 1\n");
 			// 1. Prepares ingredients 
-			
 			raw_x = (double)x;
 			raw_y = y_scale * sin(raw_x * x_scale);
 			y = (int)raw_y;
 		
-			printf("Debug: Animation Loop 2\n");
 			// 2. Put it in the buffer
 			intPush(len, &y, buffer);
 
-			printf("[out of buffer - %d]", y); 
-
-			// Debug - check the buffer
-			for (int i = 0; i < len; i++) {
-				printf("%d", buffer[i]);
-			}
-			printf("\n");
-
-			printf("Debug: Animation Loop 3\n");
 			// 3. Draw the trajectory
 			drawTrajectory(width, height, canvas, coord_x, coord_y, len, buffer, y_scale);
 
-			printf("Debug: Animation Loop 4\n");
 			// 4. Print the canvas
 			printCanvas(width, height, canvas);
 
-			printf("Debug: Animation Loop 5\n");
 			// 5. Reset the canvas
 			makeCanvas(width, height, canvas); 
 
 			// Increment
 			x++; 	
 
-			// Functionals
-			how_long--; 
+			// Functionals 
 			clkSpeed(15.0); 
 		}
 
